fe3cc48b8f518684b9a97b2960c7bface2995b30
Andreas Itten <andreas.itten@hispeed.ch>
2024-11-26 21:21:19 +0100

    Added BETAFPVF411 Servo Support

--- src/main/target/BETAFPVF411/target.c
+++ src/main/target/BETAFPVF411/target.c
@@ -27,8 +27,11 @@
 timerHardware_t timerHardware[] = {
     DEF_TIM(TIM3,  CH1, PB4,    TIM_USE_OUTPUT_AUTO, 0, 0),
     DEF_TIM(TIM3,  CH2, PB5,    TIM_USE_OUTPUT_AUTO, 0, 0),
-    DEF_TIM(TIM4,  CH1, PB6,    TIM_USE_OUTPUT_AUTO, 0, 0),
-    DEF_TIM(TIM4,  CH2, PB7,    TIM_USE_OUTPUT_AUTO, 0, 0),
+//    DEF_TIM(TIM4,  CH1, PB6,    TIM_USE_OUTPUT_AUTO, 0, 0),
+//    DEF_TIM(TIM4,  CH2, PB7,    TIM_USE_OUTPUT_AUTO, 0, 0),
+    
+    DEF_TIM(TIM2,  CH3, PA2,    TIM_USE_OUTPUT_AUTO, 0, 0),
+    DEF_TIM(TIM2,  CH4, PA3,    TIM_USE_OUTPUT_AUTO, 0, 0),
 };
 
 const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
